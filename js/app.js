9ef6979e0740f14d36ede3887c28256c78c9da75
Fabián Huertas <miloh9318@hotmail.com>
2025-09-11 19:52:34 -0500

    primeros pasos funcion guardarCliente

--- js/app.js
+++ js/app.js
@@ -4,3 +4,19 @@ let cliente = {
     pedido: []
 };
 
+const btnGuardarCliente = document.querySelector('#guardar-cliente');
+btnGuardarCliente.addEventListener('click', guardarCliente);
+
+function guardarCliente() {
+    const mesa = document.querySelector('#mesa').value;
+    const hora = document.querySelector('#hora').value;
+    
+    //Revisar si hay campos vacios
+    const camposVacios = [mesa, hora].some( campo => campo === '');
+
+    if(camposVacios){
+        console.log('Hay almenos un campo vacio');
+    } else{
+        console.log('Todos los campos estan llenos');    
+    }
+}
