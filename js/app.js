a881d8f713932146ede5b5c32428963fb97589a1
Fabián Huertas <miloh9318@hotmail.com>
2025-09-24 20:49:48 -0500

    mensajes condicionales segun el pedido del usuario

--- js/app.js
+++ js/app.js
@@ -143,8 +143,13 @@ function agregarPlatillo(producto){
     //Limpiar HTML previo
     limpiarHTML();
 
-    // Mostrar el resumen
-    actualizarResumen();
+    if(cliente.pedido.length) {
+        // Mostrar el resumen
+        actualizarResumen();
+    }else {
+        mensajePedidoVacio();
+    }
+
 }
 
 function actualizarResumen(){
@@ -278,6 +283,25 @@ function eliminarProducto(id) {
     //Limpiar HTML previo
     limpiarHTML();
 
-    // Mostrar el resumen
-    actualizarResumen();
+    if(cliente.pedido.length) {
+        // Mostrar el resumen
+        actualizarResumen();
+    }else {
+        mensajePedidoVacio();
+    }
+
+    // El producto se elimino por lo tanto  reiniciamos la cantidad a 0 en el formulario
+    const productoEliminado = `#producto-${id}`;
+    const inputEliminado = document.querySelector(productoEliminado);
+    inputEliminado.value = 0;
+}
+
+function mensajePedidoVacio() {
+    const contenido = document.querySelector('#resumen .contenido');
+
+    const texto = document.createElement('P');
+    texto.classList.add('text-center');
+    texto.textContent = 'Añade los elementos del pedido';
+
+    contenido.appendChild(texto);
 }
