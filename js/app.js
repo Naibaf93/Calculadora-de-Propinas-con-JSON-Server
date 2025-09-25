f5086eb4cdcbad43d6692c56aa4e62708d03a9e0
Fabián Huertas <miloh9318@hotmail.com>
2025-09-24 21:51:37 -0500

    creando el formulario de propinas

--- js/app.js
+++ js/app.js
@@ -156,7 +156,7 @@ function actualizarResumen(){
     const contenido = document.querySelector('#resumen .contenido');
 
     const resumen = document.createElement('DIV');
-    resumen.classList.add('col-md-6', 'card', 'py-5', 'px-3', 'shadow');
+    resumen.classList.add('col-md-6', 'card', 'py-2', 'px-3', 'shadow');
 
      // Informacion mesa
     const mesa = document.createElement('P');
@@ -255,12 +255,15 @@ function actualizarResumen(){
     })
 
     //Agregar al contenido
+    resumen.appendChild(heading);
     resumen.appendChild(mesa);
     resumen.appendChild(hora);
-    resumen.appendChild(heading);
     resumen.appendChild(grupo);
 
     contenido.appendChild(resumen);
+
+    // Mostrar formulario de propinas
+    formularioPropinas();
 }
 
 function limpiarHTML(){
@@ -304,4 +307,24 @@ function mensajePedidoVacio() {
     texto.textContent = 'Añade los elementos del pedido';
 
     contenido.appendChild(texto);
+}
+
+function formularioPropinas(){
+    
+    const contenido = document.querySelector('#resumen .contenido');
+
+    const formulario = document.createElement('DIV');
+    formulario.classList.add('col-md-6', 'formulario');
+
+    const divFormulario = document.createElement('DIV');
+    divFormulario.classList.add('card', 'py-2', 'px-3', 'shadow');
+
+    const heading = document.createElement('H3');
+    heading.classList.add('my-4', 'text-center');
+    heading.textContent = 'Propina';
+
+    divFormulario.appendChild(heading);
+    formulario.appendChild(divFormulario);
+    
+    contenido.appendChild(formulario);
 }
